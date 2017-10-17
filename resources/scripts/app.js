cea417a04c291c9b500770db02e92771c64e5ad3
Iqbal Mohammad Abdul Ghoni <iqballmags@gmail.com>
2017-10-18 02:47:39 +0700

    Fix code highlighting

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -3,3 +3,11 @@ $('#return-to-top').click(function() {      // When arrow is clicked
     scrollTop : 0                       // Scroll to top of body
   }, 1000);
 });
+
+hljs.initHighlightingOnLoad();
+
+$(document).ready(function(){
+  $('code.hljs').each(function(i, block) {
+    hljs.lineNumbersBlock(block);
+  });
+});
