11533f6d217dfe52ab1168865bbf7dd5ed2cd3e1
Iqbal Mohammad Abdul Ghoni <iqballmags@gmail.com>
2017-10-18 13:23:42 +0700

    Add Share buttons

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -10,4 +10,10 @@ $(document).ready(function(){
   $('code.hljs').each(function(i, block) {
     hljs.lineNumbersBlock(block);
   });
+
+  $('.share-button').click(function(e) {
+    e.preventDefault();
+    window.open($(this).attr('href'), 'fbShareWindow', 'height=500, width=320, top=' + ($(window).height() / 2 - 275) + ', left=' + ($(window).width() / 2 - 225) + ', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
+    return false; 
+  });
 });
